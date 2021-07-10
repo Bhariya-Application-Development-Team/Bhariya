c4af1e9a1c3ca256e931cd2aa839c236f397f9e2
Sujal Singh Thakuri <sujalthakuribe@gmail.com>
2021-07-10 16:34:55 +0530

    Forgot Password Activities Created

---
+++ app/src/main/java/com/sudhir/bhariya/CodeVerificationActivity.kt
@@ -0,0 +1,11 @@
+package com.sudhir.bhariya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CodeVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_verification)
+    }
+}
