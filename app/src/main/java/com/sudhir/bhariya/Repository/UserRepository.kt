49b0e36c5f2a62d986129a9c1ecd2b229ed2d23e
sudhirkharel05 <sudhir.kharel.52@gmail.com>
2021-07-14 17:51:39 +0545

    ProfileActivity code corrected

--- app/src/main/java/com/sudhir/bhariya/Repository/UserRepository.kt
+++ app/src/main/java/com/sudhir/bhariya/Repository/UserRepository.kt
@@ -31,7 +31,7 @@ class UserRepository : MyApiRequest(){
         }
     }
 
-}
+
     suspend fun  viewUser(): UserResponse {
         return apiRequest {
             userAPI.viewUser(ServiceBuilder.token!!)
