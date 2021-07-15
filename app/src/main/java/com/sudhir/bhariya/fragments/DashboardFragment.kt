ac1f11fde03755353c6b862ba284036aacb17892
sudhirkharel05 <sudhir.kharel.52@gmail.com>
2021-07-15 18:55:15 +0545

    UI for Dashboard created

--- app/src/main/java/com/sudhir/bhariya/fragments/DashboardFragment.kt
+++ app/src/main/java/com/sudhir/bhariya/fragments/DashboardFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.sudhir.bhariya.R
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
+import java.util.*
 
 class DashboardFragment : Fragment() {
+    var sampleImages = intArrayOf(
+        R.drawable.logo,
+        R.drawable.logo,
 
+        )
 
 
     override fun onCreateView(
@@ -16,8 +24,26 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
-    }
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
+        val carouselView = root.findViewById(R.id.carouselviews) as CarouselView;
+
+
+            carouselView.setImageListener(imageListener);
+        carouselView.setPageCount(sampleImages.size);
+
+
+
+        return root
+
+    }
+    var imageListener: ImageListener = object : ImageListener {
+        override fun setImageForPosition(position: Int, imageView: ImageView) {
+            // You can use Glide or Picasso here
+
+            imageView.setImageResource(sampleImages[position])
+
+        }
+    }
 
 }
