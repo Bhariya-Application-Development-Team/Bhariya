63aac9852473c81577e31daec892db4f556cac8a
Sabin Simkhada <sbnsim81@gmail.com>
2021-07-17 14:32:15 +0545

    Driver registration Design

--- app/src/main/java/com/sudhir/bhariya/fragments/SettingFragment.kt
+++ app/src/main/java/com/sudhir/bhariya/fragments/SettingFragment.kt
@@ -1,11 +1,23 @@
 package com.sudhir.bhariya.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sudhir.bhariya.R
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
+import com.sudhir.bhariya.*
+import com.sudhir.bhariya.Repository.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class SettingFragment : Fragment() {
@@ -20,3 +32,4 @@ class SettingFragment : Fragment() {
 
 
 }
+
