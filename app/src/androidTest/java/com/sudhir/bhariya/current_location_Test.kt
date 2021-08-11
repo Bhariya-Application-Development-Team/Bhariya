87983bfa1484b440c5bcc01295e741c03b612ba9
wangchhu <wangchhutamang123@gmail.com>
2021-08-11 21:48:56 +0545

    current location tested

---
+++ app/src/androidTest/java/com/sudhir/bhariya/current_location_Test.kt
@@ -0,0 +1,134 @@
+package com.sudhir.bhariya
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class current_location_Test {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(LoginActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"
+        )
+
+    @Test
+    fun current_location_Test() {
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.etphonenumber),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.phonenumbertxt),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("98"), closeSoftKeyboard())
+
+        val appCompatImageView = onView(
+            allOf(
+                withId(R.id.btnlogin),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        4
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageView.perform(click())
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.ic_notification), withContentDescription("Notification"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.botton_nav),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val imageView = onView(
+            allOf(
+                withContentDescription("My Location"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.FrameLayout")),
+                        2
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        imageView.perform(click())
+
+        val imageView2 = onView(
+            allOf(
+                withContentDescription("My Location"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.FrameLayout")),
+                        2
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        imageView2.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
