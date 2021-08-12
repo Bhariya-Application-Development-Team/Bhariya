96ab742a1d0f28bae7b6bd38ee4494335333868e
Sabin Simkhada <sbnsim81@gmail.com>
2021-08-12 20:10:11 +0545

    Feedback Activity Created

--- app/src/main/java/com/sudhir/bhariya/EsewaActivity.kt
+++ app/src/main/java/com/sudhir/bhariya/EsewaActivity.kt
@@ -1,11 +1,61 @@
 package com.sudhir.bhariya
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import com.esewa.android.sdk.payment.ESewaConfiguration
+import com.esewa.android.sdk.payment.ESewaPayment
+import com.esewa.android.sdk.payment.ESewaPaymentActivity
 
 class EsewaActivity : AppCompatActivity() {
+
+    private lateinit var pay: ImageButton
+    private var REQUEST_CODE_PAYMENT: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_esewa)
+
+        pay = findViewById(R.id.pay)
+
+        pay.setOnClickListener {
+            val eSewaPayment: ESewaPayment = ESewaPayment(
+                "900",
+                "Truck Heavy",
+                "abjbsdakjsb121",
+                "https://60efeb43a276b8d5e8ceaa9f--jolly-torvalds-099307.netlify.app/"
+            )
+
+            val intent: Intent = Intent(this, ESewaPaymentActivity::class.java)
+            intent.putExtra(ESewaConfiguration.ESEWA_CONFIGURATION, eSewaConfiguration)
+
+            intent.putExtra(ESewaPayment.ESEWA_PAYMENT, eSewaPayment)
+            startActivityForResult(intent, REQUEST_CODE_PAYMENT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                val s = data!!.getStringExtra(ESewaPayment.EXTRA_RESULT_MESSAGE)
+                if (s != null) {
+                    Log.i("Proof of Payment", s)
+                }
+                Toast.makeText(this, "SUCCESSFUL PAYMENT", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Canceled By User", Toast.LENGTH_SHORT).show()
+            } else if (resultCode == ESewaPayment.RESULT_EXTRAS_INVALID) {
+                val s = data!!.getStringExtra(ESewaPayment.EXTRA_RESULT_MESSAGE)
+                if (s != null) {
+                    Log.i("Proof of Payment", s)
+                }
+            }
+        }
     }
 }
